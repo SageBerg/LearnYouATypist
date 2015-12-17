3e478d59a493ad97e13cee3fbf9d8e3433f2a8ce
Sage Berg <sberg@uoregon.edu>
2015-12-17 14:37:21 -0800

    now removes more newlines and replaces accented characters

--- make_lessons.py
+++ make_lessons.py
@@ -27,11 +27,23 @@ def write_lesson(text, file_series, number):
     f.close()
 
 def clean(text):
+    text = text.replace("\n\n", "\n")
     text = text.replace("’", "\'")
     text = text.replace("“", "\"")
     text = text.replace("”", "\"")
     text = text.replace("–", "-") # en dash
     text = text.replace("—", "-") # em dash
+    text = text.replace("à", "a")
+    text = text.replace("À", "A")
+    text = text.replace("é", "e")
+    text = text.replace("É", "E")
+
+    text = text.replace("í", "i")
+    text = text.replace("Í", "I")
+    text = text.replace("ó", "o")
+    text = text.replace("Ó", "O")
+    text = text.replace("ú", "u")
+    text = text.replace("Ú", "U")
     return text
 
 make_lesson()
